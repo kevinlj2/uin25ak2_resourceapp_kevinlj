33c8786c5342abc6528f97e003c8d16e65a4df83
kevinlj2 <kevlok4@gmail.com>
2025-03-12 20:52:41 +0100

    endret fra buttons til navlink

--- src/assets/components/Nav.jsx
+++ src/assets/components/Nav.jsx
@@ -1,27 +1,21 @@
+import { NavLink } from "react-router";
+import { resources } from "../js/ressurser.js";
 import "../styles/nav.scss";
 
 export default function Nav() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <button id="HTML" class="active">
-            HTML
-          </button>
-        </li>
-        <li>
-          <button id="CSS">CSS</button>
-        </li>
-        <li>
-          <button id="JavaScript">JavaScript</button>
-        </li>
-        <li>
-          <button id="React">React</button>
-        </li>
-        <li>
-          <button id="Sanity">Sanity and headless CMS</button>
-        </li>
-      </ul>
-    </nav>
+    <header>
+      <nav>
+        <ul>
+          {resources.map((resource, index) => (
+            <li key={index}>
+              <NavLink to={`/${resource.category}`} className="navLink">
+                {resource.category}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
   );
 }
--- src/assets/components/Resources.jsx
+++ src/assets/components/Resources.jsx
@@ -1,3 +1,30 @@
+import "../styles/resources.scss";
+import { resources } from "../js/ressurser.js";
+import { useParams } from "react-router";
+
 export default function Resources() {
-  return;
+  const { category } = useParams();
+
+  const filteredResources = resources.filter(
+    (resource) => resource.category === category
+  );
+
+  console.log(resources);
+  return (
+    <section>
+      {filteredResources.map((resource, index) => (
+        <article key={index}>
+          <ul>
+            {resource.sources.map((source, idx) => (
+              <li key={idx}>
+                <a href={source.url} _target="_blank">
+                  {source.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </article>
+      ))}
+    </section>
+  );
 }
